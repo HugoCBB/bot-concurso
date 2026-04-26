e1c92a1620688585e7f5d7d2e3952f2432cea7b4
HugoCBB <hugocarlosb.071@gmail.com>
2026-04-26 20:03:07 -0300

    .

--- app/routes/contests_routes.py
+++ app/routes/contests_routes.py
@@ -1,17 +1,13 @@
 from modules.utils.file_dirs import FILE_JSON_DIR
 from fastapi_pagination import Page, add_pagination, paginate
 from fastapi import APIRouter
+from infra.redis_client import get_contests
 
-import json
-
-
-FILE_JSON = FILE_JSON_DIR / "data.json"
 route = APIRouter()
 
 @route.get("/", response_model=Page[dict])
 async def get_all_contest():
-    with open(FILE_JSON, "r", encoding='utf-8') as file:
-        data = json.load(file)
+    data = await get_contests()
     return paginate(data)
 add_pagination(route)
 
