1562ba0bd1ce761b2168385b2d7abda6553998c0
hugocbb <hugocarlosb.071@gmail.com>
2026-07-16 21:50:25 -0300

    fix: update S3 configuration to use Optional types for better handling of environment variables

--- app/modules/config/config.py
+++ app/modules/config/config.py
@@ -1,5 +1,6 @@
 from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
+from typing import Optional
 import os
 
 load_dotenv()
@@ -13,10 +14,10 @@ class __Settings(BaseSettings):
 
     cors_url: str = os.getenv("CORS_URL") or "*"
 
-    s3_endpoint: str = os.getenv("S3_ENDPOINT")
+    s3_endpoint: Optional[str] = os.getenv("S3_ENDPOINT")
     s3_region: str = os.getenv("S3_REGION") or "us-west-1"
-    s3_acess_key: str = os.getenv("S3_ACCESS_KEY")
-    s3_secret_key: str = os.getenv("S3_SECRET_KEY")
+    s3_acess_key: Optional[str] = os.getenv("S3_ACCESS_KEY")
+    s3_secret_key: Optional[str] = os.getenv("S3_SECRET_KEY")
     s3_bucket: str = os.getenv("S3_BUCKET") or "editais-bot-concurso"
 
     class Config:
